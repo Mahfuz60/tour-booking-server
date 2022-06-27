7d52f0aa61737df34f0996ac326dccfe86720c22
mahfuz60 <mahfuzuralam1996@gmail.com>
2022-06-28 03:05:16 +0600

    create post method

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import authRoute from './routes/auth.js';
+import authRoute from './routes/authRoute.js';
+import usersRoute from './routes/usersRoute.js';
+import hotelsRoute from './routes/hotelsRoute.js';
+import hotelRoomRoute from './routes/hotelRoomRoute.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 dotenv.config();
-// Connection URI
-const url = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.udmgcdk.mongodb.net/?retryWrites=true&w=majority`;
 
 //Basic Setup Mongoose
 const connect = async () => {
   try {
-    await mongoose.connect(url);
+    await mongoose.connect(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true });
   } catch (error) {
     throw error;
   }
@@ -24,7 +25,10 @@ mongoose.connection.on('disconnected', () => {
 //middleware
 app.use(express.json());
 app.use(cors());
-app.use('/auth', authRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/hotelRoom', hotelRoomRoute);
 
 app.listen(PORT, () => {
   connect();
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('hello,this is user auth');
+  res.send('hello,this is  auth');
 });
 
 export default router;
