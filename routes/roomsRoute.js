11eb5a9717f99da81d87143d43eabe402a0f43d8
mahfuz60 <mahfuzuralam1996@gmail.com>
2022-07-01 20:58:34 +0600

    hotel room

--- routes/roomsRoute.js
+++ routes/roomsRoute.js
@@ -10,9 +10,9 @@ router.post('/:hotelid', verifyAdmin, createRoom);
 //UPDATE
 router.put('/:id', verifyAdmin, updateRoom);
 //DELETE
-router.delete('/:id', verifyAdmin, deleteRoom);
+router.delete('/:id/:hotelid', verifyAdmin, deleteRoom);
 //GET
-router.get('/:id', getRoom);
+router.get('/:id', getRoom); 
 //GET ALL
 router.get('/', getAllRoom);
 
